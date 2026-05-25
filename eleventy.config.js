e7b5185d76720727d5eb2a1ff495873ce0c19116
damalga <damalga@protonmail.com>
2026-05-25 18:22:13 +0200

    update  eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,6 +3,8 @@ import Handlebars from 'handlebars';
 import felidImage from './src/helpers/felid-image.js';
 import randomFelids from './src/helpers/random-felids.js';
 import getFelidData from './public/data/felids-wikidata.js';
+import enrichWithLinks from './public/data/felids-links.js';
+import enrichWithImages from './public/data/felids-images.js';
 
 export default async function(eleventyConfig) {
   // Register Handlebars helpers BEFORE adding the plugin
@@ -12,8 +14,7 @@ export default async function(eleventyConfig) {
   // Add Handlebars plugin
   eleventyConfig.addPlugin(handlebarsPlugin);
 
-  // Add global data from Wikidata (fetches images at build time)
-  const felidsData = await getFelidData();
+  const felidsData = await enrichWithImages(await enrichWithLinks(await getFelidData()));
   eleventyConfig.addGlobalData('felids', felidsData);
 
   // Copy assets to output
@@ -22,6 +23,10 @@ export default async function(eleventyConfig) {
 
   // Copy public assets to root of dist
   eleventyConfig.addPassthroughCopy({ "public/assets": "assets" });
+
+  // Copy glider-js from node_modules
+  eleventyConfig.addPassthroughCopy({ "node_modules/glider-js/glider.min.js": "assets/libs/glider/glider.min.js" });
+  eleventyConfig.addPassthroughCopy({ "node_modules/glider-js/glider.min.css": "assets/libs/glider/glider.min.css" });
 
   // Set input/output directories
   return {
